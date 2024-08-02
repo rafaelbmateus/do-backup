a132ec5c686365d00cf0698f98eba9fee1bc0296
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2024-08-02 13:23:32 -0300

    fix: rotate image

--- scripts/rotate.sh
+++ scripts/rotate.sh
@@ -4,9 +4,11 @@ source slack.sh
 
 BACKUPS_TO_KEEP=10
 
-ALL_BACKUPS=$(s3cmd ls ${DO_SPACE}/${DO_FOLDER}/ | grep "backup_" | sort | awk '{print $4}')
+ALL_BACKUPS=$(s3cmd ls ${DO_SPACE}/${DO_FOLDER}/ | sort | awk '{print $4}')
 BACKUP_COUNT=$(echo "$ALL_BACKUPS" | wc -l)
 BACKUPS_TO_DELETE=$((BACKUP_COUNT - BACKUPS_TO_KEEP))
+echo "Rotate backup..."
+echo "Backup count $BACKUP_COUNT to delete $BACKUPS_TO_DELETE"
 if [ $BACKUPS_TO_DELETE -gt 0 ]; then
   BACKUPS_TO_REMOVE=$(echo "$ALL_BACKUPS" | head -n $BACKUPS_TO_DELETE)
   for BACKUP in $BACKUPS_TO_REMOVE; do
